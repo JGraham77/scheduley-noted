8c449c2b665206b0a83729831d2c305876bf2767
Joshua Graham <joshuareynoldsgraham@gmail.com>
2023-10-04 16:26:40 -0500

    beginning client side as well as testing functionality

---
+++ src/client/views/Verify.tsx
@@ -0,0 +1,18 @@
+import React, { useEffect } from "react";
+
+const Verify = () => {
+    // useEffect(() => {
+    //     async function checkEm() {
+    //         const url = new URL(window.location.href);
+    //         const code = url.searchParams.get("code");
+    //         const res = await fetch("https://localhost:3000/auth/email/verify?code=" + code);
+    //         const data = res.json();
+    //         if (res.ok) {
+    //             alert(data.token);
+    //         }
+    //     }
+    // });
+    return <div>Verify</div>;
+};
+
+export default Verify;
--- src/server/db/queries/users.ts
+++ src/server/db/queries/users.ts
@@ -9,9 +9,10 @@ interface NewUser {
     phone: string;
 }
 
-type columns = "email" | "username";
+type columns = "email" | "username" | "id";
 
-const by = (column: columns, value: string) => Query<User[]>("SELECT * FROM USERS WHERE $1=$2", [column, value]);
+const by = (column: columns, value: string | number) =>
+    Query<User[]>("SELECT * FROM USERS WHERE $1=$2", [column, value]);
 
 const register = ({ name, email, username, password, phone }: NewUser) =>
     Query(`INSERT INTO Users (name, email, username, password, phone) VALUES ($1,$2,$3,$4,$5)`, [
