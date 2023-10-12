b1a1ec26b36b0e34c19a57406560abc45a165b02
Joshua Graham <joshuareynoldsgraham@gmail.com>
2023-10-12 14:32:56 -0500

    adding controllers to routes

--- src/server/routes/auth/register.ts
+++ src/server/routes/auth/register.ts
@@ -1,53 +1,8 @@
 import express from "express";
-import db from "../../db";
-import utils from "../../utils";
-import { sendVerificationEmail } from "../../services/email";
+import controllers from "../../controllers";
 
 const router = express.Router();
 
-router.post("/", async (req, res) => {
-    const { name, email, username, password, phone } = req.body;
-
-    const missingProperties = utils.validators.checkForMissingProperties({ name, email, username, password, phone });
-
-    if (missingProperties) {
-        return res.status(400).json({ message: "Missing a property", missingProperties });
-    }
-
-    if (
-        !utils.validators.allStringsAreGood([
-            [name, 64],
-            [username, 32],
-            [email, 128],
-            [phone, 16],
-            [password, 1000],
-        ])
-    ) {
-        return res.status(400).json({ message: "Some values are not strings or exceed their max length" });
-    }
-
-    if (!utils.validators.isEmail(email)) {
-        res.status(400).json({ message: "Please use a valid email" });
-        return;
-    }
-
-    if (utils.validators.isEmail(username)) {
-        res.status(400).json({ message: "Username cannot be an email." });
-        return;
-    }
-
-    try {
-        const hashed = await utils.passwords.slinging_slasher(password);
-        const result = await db.users.register({ name, email, username, password: hashed, phone });
-        await sendVerificationEmail(result.insertId!, email);
-
-        res.status(201).json({
-            message: "Successfully registered!  Please check your email to verify your account!",
-            id: result.insertId,
-        });
-    } catch (error) {
-        res.status(500).json({ message: "Server's broke" });
-    }
-});
+router.post("/", controllers.auth.register);
 
 export default router;
